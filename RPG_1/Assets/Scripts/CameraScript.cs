ec45d93b2835a44466f73b9462bb7132cc206ea3
guilhermescosta <guilhermescosta@gmail.com>
2019-07-04 23:42:08 -0300

    04 jul

---
+++ RPG_1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public bool isLooking;
+
+    public float speedX;
+    public float speedY;
+
+    public float axisX;
+    public float axisY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLooking) {
+            Look();
+        }
+    }
+
+    public void Look() {
+        axisX += speedX * Input.GetAxis("Mouse X");
+        axisY -= speedY * Input.GetAxis("Mouse Y");
+
+        axisX = Mathf.Clamp(axisX, -90f, 90f);
+        axisY = Mathf.Clamp(axisY, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(axisY, axisX, 0.0f);
+    }
+}
---
+++ RPG_1/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level;
+    public string playerName;
+
+    [Range(0,100)]
+    public int strength;
+    [Range(0, 100)]
+    public int dexterity;
+    [Range(0, 100)]
+    public int intelligence;
+    
+
+    public int hp;
+    public int mp;
+
+    public int xp;
+
+
+    public Animator _anim;
+    // Start is called before the first frame update
+
+  
+        void Start()
+    {
+        //warrior
+        level = 1;
+        strength = 10;
+        dexterity = 5;
+        intelligence = 1;
+        hp = 10;
+        mp = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(0, 0, 0.05f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.02f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+    }
+
+    public void Attack() {
+        _anim.SetTrigger("Attack_Move");
+    }
+}
