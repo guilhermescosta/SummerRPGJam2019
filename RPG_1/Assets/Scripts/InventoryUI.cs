b1ca06b0ab79b37048a408257c119b47f44ec910
guilhermescosta <guilhermescosta@gmail.com>
2019-07-15 20:35:47 -0300

    atualização do inventario
    item apresentado na ui do inventario

---
+++ RPG_1/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    Inventory _inventory;
+    public Transform itemsParent;
+
+    Slot[] slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inventory = Inventory.instance;
+
+        slots = itemsParent.GetComponentsInChildren<Slot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateUI() {
+      
+        for (int i = 0; i < slots.Length; i++) {
+            if (i < _inventory.items.Count)
+            {
+                slots[i].AddItem(_inventory.items[i]);
+            }
+            else {
+                slots[i].ClearSlot();
+            }
+
+        }
+
+    }
+
+
+}
